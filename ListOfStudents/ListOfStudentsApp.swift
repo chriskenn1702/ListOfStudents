c10ad9905af17bf8170309507933111033a5c72d
Christopher Kennedy <kennednu@bc.edu>
2023-03-27 16:50:06 -0400

    Initial Commit

---
+++ ListOfStudents/ListOfStudentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfStudentsApp.swift
+//  ListOfStudents
+//
+//  Created by Christopher Kennedy on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ListOfStudentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
